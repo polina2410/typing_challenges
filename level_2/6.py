49cfa7cc0dc3e5a835162e8a2dbdda0f642cc90a
Polina <polinasmekhova@gamil.com>
2024-02-29 23:28:38 +0100

    Fixed type hints: level_2

--- level_2/6.py
+++ level_2/6.py
@@ -1,7 +1,7 @@
 from constants import ___
 
 
-def is_name_male(name: str, name_gender_map: dict[str, bool]) -> bool:
+def is_name_male(name: str, name_gender_map: dict[str, bool]) -> bool | None:
     pass
 
 
